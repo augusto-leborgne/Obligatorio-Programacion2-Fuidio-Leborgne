c10beac503344895cc4965641dbb1d4c3b598595
Mateo <jfuidio@correo.um.edu.uy>
2024-06-03 11:12:57 -0300

    aj

--- src/uy/edu/um/main/Main.java
+++ src/uy/edu/um/main/Main.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        Reports report = new Reports();
+        for (int i = 0; i < report.top10("BG","2024-05-13").size(); i++) {
+            System.out.println(report.top10("BG", "2024-05-13").get(i));
+        }
     }
 }
