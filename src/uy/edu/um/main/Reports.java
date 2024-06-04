980ffe3edf032ea71000abef818abd56cf734917
Mateo <jfuidio@correo.um.edu.uy>
2024-06-04 10:57:19 -0300

    aj

--- src/uy/edu/um/main/Reports.java
+++ src/uy/edu/um/main/Reports.java
@@ -13,37 +13,40 @@ import java.util.ArrayList;
 public class Reports {
 
     public ArrayList<String> top10(String p, String d) {
-        MyHeapImpl<String> heap = new MyHeapImpl<String>(false);
-        MyHashImpl<String, String> hash = new MyHashImpl<String, String>();
+        MyHeapImpl<String> heap = new MyHeapImpl<>(true);
+        MyHashImpl<String, String> hash = new MyHashImpl<>();
 
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(new File("universal_top_spotify_songs.csv")));
+        try (BufferedReader in = new BufferedReader(new FileReader(new File("universal_top_spotify_songs.csv")))) {
             System.out.println("File open successful!");
 
-            //Agregar lineas a hash y daily_ranks a heap
             int line = 0;
             for (String x = in.readLine(); x != null; x = in.readLine()) {
                 line++;
                 String[] valores = x.split(",");
-                if(line != 1 && valores[6].equals(p) && valores[7].equals(d)){
+                if (line != 1 && valores[6].equals(p) && valores[7].equals(d)) {
                     hash.put(valores[3], x);
                     heap.insert(valores[3]);
                 }
             }
 
             ArrayList<String> songs = new ArrayList<>();
-            for(int i=0; i<heap.size();i++){
-                if(Integer.parseInt(heap.get()) <= 10){
-                    String k = heap.delete();
-                    songs.add(hash.get(k));
+            int count = 0;
+            while (count < 10 && heap.size() > 0) {
+                String k = heap.delete();
+                String songData = hash.get(k);
+                if (songData != null) {
+                    songs.add(songData);
+                    count++;
                 }
             }
             return songs;
         } catch (IOException e) {
             System.out.println("File I/O error!");
+            e.printStackTrace();
             return null;
         } catch (KeyNullException e) {
             System.out.println("Key Null ERROR!");
+            e.printStackTrace();
             return null;
         }
     }
